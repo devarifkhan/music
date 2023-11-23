89bdd42388e5f696daf5a2334486dbbc4e48960f
Md Ariful Islam <arifcse209@gmail.com>
2023-11-23 21:07:38 +0600

    actions implemented

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -104,8 +104,7 @@
 </template>
 
 <script>
-import { auth, userCollection } from '@/includes/firebase'
-import { mapWritableState } from 'pinia'
+import { mapActions } from 'pinia'
 import useUserStore from '@/stores/user'
 
 export default {
@@ -131,47 +130,27 @@ export default {
       reg_alert_msg: 'Please wait! Your account is being created.'
     }
   },
-  computed: {
-    ...mapWritableState(useUserStore, ['userLoggedIn'])
-  },
   methods: {
+    ...mapActions(useUserStore, {
+      createUser: 'register'
+    }),
     async register(values) {
       this.reg_show_alert = true
       this.reg_in_submission = true
       this.reg_alert_variant = 'bg-blue-500'
       this.reg_alert_msg = 'Please wait! Your account is being created.'
 
-      let userCred = null
-
       try {
-        userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
+        this.createUser(values)
       } catch (error) {
         this.reg_in_submission = false
         this.reg_alert_variant = 'bg-red-500'
         this.reg_alert_msg = `An error occurred. Please try again later.`
         return
       }
-
-      try {
-        await userCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country
-        })
-      } catch (error) {
-        this.reg_in_submission = false
-        this.reg_alert_variant = 'bg-red-500'
-        this.reg_alert_msg = `An error occurred. Please try again later.`
-        return
-      }
-
-      this.userLoggedIn = true
 
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_msg = 'Success! Your account has been created.'
-
-      console.log(userCred)
     }
   }
 }
