1408591458544872ce2ab17f1b2a05f8c4b2925e
Md Ariful Islam <arifcse209@gmail.com>
2023-11-23 19:40:20 +0600

    firebase setup

---
+++ src/includes/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBGXA0kundZrUW110xhRIPbPryz66ojJFo",
+    authDomain: "music-491ce.firebaseapp.com",
+    projectId: "music-491ce",
+    storageBucket: "music-491ce.appspot.com",
+    appId: "1:559939289933:web:891af5882a0ca34efd6d04"
+};
+
+export default firebase.initializeApp(firebaseConfig);
