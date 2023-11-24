367291b6f2e2446c5e991301d271c623033182b6
Md Ariful Islam <arifcse209@gmail.com>
2023-11-24 20:04:51 +0600

    routing complete

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,21 @@
   <header id="header" class="bg-gray-700">
     <nav class="container flex items-center justify-start px-4 py-5 mx-auto">
       <!-- App Name -->
-      <a class="mr-4 text-2xl font-bold text-white uppercase" href="#">Music</a>
+      <router-link
+        class="mr-4 text-2xl font-bold text-white uppercase"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+        >Music</router-link
+      >
 
       <div class="flex items-center flex-grow">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }">About</router-link>
+          </li>
+
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
               >Login / Register</a
@@ -17,10 +26,10 @@
 
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
             </li>
             <li>
-              <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
+              <a class="px-2 text-white" href="#" @click.prevent="signOut">Logout</a>
             </li>
           </template>
         </ul>
@@ -42,6 +51,13 @@ export default {
   methods: {
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen
+    },
+    signOut() {
+      this.userStore.signOut()
+
+      if (this.$route.meta.requiresAuth) {
+        this.$router.push({ name: 'home' })
+      }
     }
   }
 }
